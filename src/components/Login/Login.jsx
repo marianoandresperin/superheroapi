423e68965cf3934855942a59e93f7523a00a7394
Mariano Perin <mariano.perin@gmail.com>
2022-01-10 21:25:45 -0300

    fake login for deployment

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,61 +1,24 @@
 import './Login.css'
-import { Formik, Form, Field } from 'formik';
 import { useLogin } from '../../contexts/LoginContext';
 
 const Login = () => {    
-    const { formSubmit, auth, logOut } = useLogin();
+    const { logIn, auth, logOut } = useLogin();
 
     const handleLogOut = () => {
         logOut();
     }
-
-    function validateEmail(value) {
-        let error;
-        if (!value) {
-            error = 'Required';
-        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
-            error = 'Invalid email address';
-        }
-        return error;
-    }
-
-    function validatePassword(value) {
-        let error;
-        if (!value) {
-            error = 'Required';
-        }
-        return error;
-    }
     
-    const handleSubmit = (value) => {
-        formSubmit(value);
+    const handleLogIn = () => {
+        logIn();
     }
 
     return (
         auth === true ? <>
-            <h1 className="logged m-2">{localStorage.getItem('email')}</h1>
+            <h1 className="logged m-2">Welcome!</h1>
             <button type="button" className="btn btn-outline-danger m-2" onClick={handleLogOut}>Sign out</button> </>
-            : <Formik
-            initialValues={{
-                email: '',
-                password: ''
-            }}
-            onSubmit={handleSubmit}
-        >
-            {({ errors, touched }) => (
-                <Form className='d-flex flex-row validation-field'>
-                    <div className='d-flex flex-column m-2'>
-                        <Field name="email" placeholder="Email" validate={validateEmail} className="form-control" />
-                        {errors.email && touched.email && <div className='validation-text'>{errors.email}</div>}
-                    </div>
-                    <div className='d-flex flex-column m-2'>
-                        <Field name="password" type="password" placeholder="Password" validate={validatePassword} className="form-control" />
-                        {errors.password && touched.password && <div className='validation-text'>{errors.password}</div>}
-                    </div>
-                    <button type="submit" className='btn btn-outline-success m-2 login-btn'>Sign in</button>
-                </Form> 
-            )}
-        </Formik>
+            : <> 
+            <h1 className="logged m-2">Log in</h1>
+            <button type="submit" className='btn btn-outline-success m-2 login-btn' onClick={handleLogIn}>Sign in</button> </>
     )
 }
 
--- src/contexts/LoginContext.js
+++ src/contexts/LoginContext.js
@@ -1,5 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import axios from 'axios'
+import { createContext, useContext, useState } from "react";
 
 const LoginContext = createContext();
 export const useLogin = () => useContext(LoginContext);
@@ -7,34 +6,16 @@ export const useLogin = () => useContext(LoginContext);
 const LoginProvider = ({ children }) => {
     const [auth, setAuth] = useState(true)
 
-    const formSubmit = (values) => {
-        axios({
-            method: 'post',
-            url: 'http://challenge-react.alkemy.org/',
-            data: values
-        })
-        .then(res => {
-            localStorage.setItem('email', values.email)
-            localStorage.setItem('token', res.data.token)
-            setAuth(true)
-        })
-        .catch(err => {
-            alert(err)
-            console.log(err)
-        })
+    const logIn = () => {
+        setAuth(true)
     }
 
     const logOut = () => {
-        localStorage.clear()
         setAuth(false)
     }
-    
-    useEffect(() => {
-        localStorage.getItem('token') !== null ? setAuth(true) : setAuth(false)
-    }, [auth])
 
     return (
-        <LoginContext.Provider value={{ auth, formSubmit, logOut }} >
+        <LoginContext.Provider value={{ auth, logOut, logIn }} >
             {children}
         </LoginContext.Provider>
     )
